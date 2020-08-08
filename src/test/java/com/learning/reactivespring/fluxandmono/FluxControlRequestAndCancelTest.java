9a8f8dae16f4a4a1fd89db47985882f87545e8c2
techsharif <sharif.cse.hstu@gmail.com>
2020-08-08 21:10:10 +0600

    Control request and cancel

---
+++ src/test/java/com/learning/reactivespring/fluxandmono/FluxControlRequestAndCancelTest.java
@@ -0,0 +1,51 @@
+package com.learning.reactivespring.fluxandmono;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class FluxControlRequestAndCancelTest {
+
+    @Test
+    public void directRequest() {
+        Flux<Integer> integerFlux = Flux.range(1, 10).log();
+
+        integerFlux.subscribe(
+                System.out::println,
+                System.err::println,
+                () -> System.out.println("Done"),
+                subscription -> subscription.request(3)
+        );
+    }
+
+    @Test
+    public void directCancel() {
+        Flux<Integer> integerFlux = Flux.range(1, 10).log();
+
+        integerFlux.subscribe(
+                System.out::println,
+                System.err::println,
+                () -> System.out.println("Done"),
+                Subscription::cancel
+        );
+    }
+
+    @Test
+    public void customRequestAndCancel() {
+        Flux<Integer> integerFlux = Flux.range(1, 10).log();
+
+        integerFlux.subscribe(
+                new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        System.out.println(value);
+                        if (value == 4) cancel();
+                        else request(1);
+                    }
+                }
+        );
+    }
+
+
+}
